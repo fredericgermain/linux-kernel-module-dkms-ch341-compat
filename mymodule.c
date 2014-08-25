a6cfec90321be36cc44b3f382963b86e9d050f56
Terry M. Poulin <BigBoss1964@gmail.com>
2014-08-25 19:31:08 +0000

    Display logging on insmod/rmmod.
    You can just insmod ./mymodule.ko after building and then rmmod
    mymodule. Use dmesg | tail to check log.

--- mymodule.c
+++ mymodule.c
@@ -1,4 +1,17 @@
 
+#include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
+
+static int mymodule_init(void) {
+	printk("<1> %s:%s()\n", __FILE__, __FUNCTION__);
+	return 0;
+}
+module_init(mymodule_init);
+
+static void mymodule_exit(void) {
+	printk("<1> %s:%s()\n", __FILE__, __FUNCTION__);
+}
+module_exit(mymodule_exit);
